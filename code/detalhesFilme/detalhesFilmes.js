fc123f6fd86a46d99008ee699bce3cc442ea83f3
Arthur Goncalves de Moraes <gdemoraesarthur@gmail.com>
2023-09-20 23:37:27 -0300

    roleta + incio pagina detalhes

---
+++ code/detalhesFilme/detalhesFilmes.js
@@ -0,0 +1,60 @@
+var url = document.URL;
+
+var url_params = url.split('?id=') 
+var params = url_params[url_params.length-1];
+var params = params.split('?type=');
+var id = params[0];
+var type = params[1];
+console.log(id);
+console.log(type);
+
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODM5NzhhMjU4YjdkNzRmOTJkNmIwZGY1NWQ5YmI1ZCIsInN1YiI6IjY0ZThlZmU5ZTg5NGE2MDExZWY4MzRhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4XoOXpMTJlf1R-ftk0WqFUnt4X786gguycsoLdvSDb8'
+    }
+  };
+
+ 
+  
+  function criarPagina(){
+    var apiUrl = '';
+    if(type == "movie"){
+         apiUrl = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+    } else {
+         apiUrl = `https://api.themoviedb.org/3/tv/${id}?language=en-US`;
+    }
+      fetch(apiUrl, options)
+      .then(res => res.json())
+      .then(data => {
+
+        console.log(data);
+        console.log('Imprimindo produtos');
+
+        let produtos = data;
+
+        let str = '';
+        let title = '';
+        let poster = produtos.poster_path;
+        let descricao = produtos.overview;
+
+          
+          if ( produtos.name != undefined){
+            title = produtos.name;
+          } else {
+            title = produtos.title;
+          }
+          str += `<img src="https://image.tmdb.org/t/p/w500${poster}">
+                  <h5> ${title} </h5>
+                  <p> ${descricao} </p>`;
+        
+        document.getElementById('tela').innerHTML = str;
+        
+
+  });
+  }
+
+
+  criarPagina();
--- code/home/index.js
+++ code/home/index.js
@@ -16,6 +16,7 @@ fetch('https://api.themoviedb.org/3/trending/all/week?language=en-US', options)
     let str = '';
     let title = '';
     let id = 0;
+    let media_type ='';
     str += `<div class="roleta">`;
     for (let i = 0; i < data.results.length; i++) {
       let produtos = data.results[i];
@@ -25,9 +26,10 @@ fetch('https://api.themoviedb.org/3/trending/all/week?language=en-US', options)
         title = produtos.title.length > 20 ? produtos.title.substring(0, 20) + '...' : produtos.title;
       }
       id = produtos.id;
+      media_type = produtos.media_type;
       let poster = produtos.poster_path;
       str += `<div class="cards">
-              <a href="../detalhesFilme/detalhesFilmes.html?${id}">
+              <a href="../detalhesFilme/detalhesFilmes.html?id=${id}?type=${media_type}">
                 <img src="https://image.tmdb.org/t/p/w500${poster}">
                 <h5>${title}</h5>
               </a>
@@ -36,7 +38,7 @@ fetch('https://api.themoviedb.org/3/trending/all/week?language=en-US', options)
     str += `</div>
             <a class="Back" onclick="plusSlides(-4)">&#10094;</a>
             <a class="forward" onclick="plusSlides(4)">&#10095;</a>`;
-    document.getElementById('tela').innerHTML = str;
+    document.getElementById('tela_roleta').innerHTML = str;
   });
 
 
@@ -61,7 +63,7 @@ fetch('https://api.themoviedb.org/3/trending/all/week?language=en-US', options)
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slidePosition-1%20].style.display = "flex";
+    slides[(slidePosition-1)%20].style.display = "flex";
     slides[(slidePosition)%20].style.display = "flex";
     slides[(slidePosition+1)%20].style.display = "flex";
     slides[(slidePosition+2)%20].style.display = "flex";
